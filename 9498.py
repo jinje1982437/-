44e2aaa67437a124bd6a9a72e22accea07ac8615
jinje1982437 <108964309+jinje1982437@users.noreply.github.com>
2022-08-06 10:46:29 +0900

    Create 9498.py

---
+++ 9498.py
@@ -0,0 +1,15 @@
+aulfhi = int(input())
+if (aulfhi >= 90) and (aulfhi <= 100):
+    print("A")
+
+if (aulfhi >= 80) and (aulfhi <= 89):
+    print("B")
+
+if (aulfhi >= 70) and (aulfhi <= 79):
+    print("C")
+
+if (aulfhi >= 60) and (aulfhi <= 69):
+    print("D")
+
+if (aulfhi >= 0) and (aulfhi <= 59):
+    print("F")
