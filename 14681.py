901dc2c33955cde9f40297403a31bf5c81d78a44
jinje1982437 <108964309+jinje1982437@users.noreply.github.com>
2022-08-13 10:49:55 +0900

    Create 14681.py

---
+++ 14681.py
@@ -0,0 +1,15 @@
+
+aulfhi = int(input())
+nvcb = int(input())
+
+if (aulfhi > 0) and (nvcb > 0):
+    print("1")
+
+if (aulfhi < 0) and (nvcb > 0):
+    print("2")
+
+if (aulfhi < 0) and (nvcb < 0):
+    print("3")
+
+if (aulfhi > 0) and (nvcb < 0):
+    print("4")
