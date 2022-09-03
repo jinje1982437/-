6dd45a37ecc2b38acdf61573c178e87c5bb022ff
jinje1982437 <108964309+jinje1982437@users.noreply.github.com>
2022-09-03 10:57:12 +0900

    Create 10951.py

---
+++ 10951.py
@@ -0,0 +1,10 @@
+try:
+
+    aj = 1
+    while aj < 10:
+        hgn, Eiu = input().split()
+        hgn = int(hgn)
+        Eiu = int(Eiu)
+        print(hgn + Eiu)
+except:
+    swnsiuldfieuliue + 103913841348130489
